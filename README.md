46950216eae76a9ec2eddb22e6dffa4832948901
Daniel Schick <schick@mbi-berlin.de>
2022-08-02 23:09:17 +0200

    restructure

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# VAT_GateValve-010
+# PyTango Device Server for Valve State Detection
+
+Provide the state of a generic valve by reading one or two switches via TangoAttributes
+e.g. from a DAQ-device.
+
+The switches can be configured as normally closed (NCC) or normally open (NOC).
+
+If only one switch is provided it directly defines the two states OPEN or CLOSED.
+
+If two switches are given they define the two states OPEN and CLOSED if they have the
+opposite values.
+If both inputs have the same value the valve is neither OPEN or CLOSED but in an undefined
+MOVING state.
--- ValveDetector.py
+++ ValveDetector.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
-import tango
 
-from tango import AttrWriteType, DevState, DispLevel, DeviceProxy, Attr, READ, AttributeProxy
-from tango.server import Device, attribute, command, device_property
+from tango import DevState, AttributeProxy
+from tango.server import Device, device_property
 
 class ValveDetector(Device):
     # Properties representing two ports of an DAQ-Device
     Input1 = device_property(
-        dtype="str",
-        default_value="imaging/DAQDevice/typeAll/A0",
-        doc='FQDN/Porttype+Portnumber',
+        dtype='str',
+        doc='FQDN to Tango Attribute for Input1:\n'
+        'domain/family/member/attribute',
     )
-    
+
     Input2 = device_property(
+        dtype='str',
+        doc='Optional\nFQDN to Tango Attribute for Input2:\n'
+        'domain/family/member/attribute',
+    )
+
+    InputType = device_property(
         dtype="str",
-        default_value="imaging/DAQDevice/typeAll/A1",
-        doc='FQDN/Porttype+Portnumber',
-    ) 
-    
-    
+        default_value='0',
+        doc='0 - normally open (NCC); 1 - normally closed (NOC)',
+    )
+
     def init_device(self):
         #set up connection to a running device server 
         Device.init_device(self)
         self.set_state(DevState.INIT)
-        self.set_status('Initialization of valve device.')
+        self.info_stream('Initialization of valve device.')
         try:
             #open a attribute proxy. That means we are accessing a specific device server attribute of an running device. 
             self.input1 = AttributeProxy(self.Input1)
@@ -55,9 +59,7 @@ class ValveDetector(Device):
             self.set_status('Valve is open.')
         else:
             self.set_status('Check if everything is ok at the valve.')
-            self.set_state(DevState.ALARM)   
-        
-        
+            self.set_state(DevState.ALARM) 
 
 
 if __name__ == '__main__':
